c92bdc1e93e0917db3ede0c294bae52d3d7b1de6
Martin Staykov <49690101+TinStay@users.noreply.github.com>
2021-02-07 14:38:36 +0100

    Changed project card styling

--- src/pages/TastyHamburger/index.md
+++ src/pages/TastyHamburger/index.md
@@ -4,7 +4,7 @@ path: "/tastyburger"
 siteURL: "none"
 githubRepo: "https://github.com/TinStay/TastyBurger"
 technologies: ["React", "Bootstrap", "Firebase"]
-shortDescription: "A web application where you can make your own burger and order it."
+shortDescription: "Choose the ingredients of your tasty burger and order it."
 mainImage: "./images/mainBurger.png"
 ---
 Burger builder website where users can put different ingredients in their burger and then order it. The price of a burger depends on the ingredients added and removed. It has been built for both desktop and mobile view. An orders page has been added where users can check previous orders. A feature where the user will be able to have the choice between making his own burger or selecting one from the displayed ones with be available soon.
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,29 +9,41 @@ const Projects = ({ data }) => {
     <Layout>
       <SEO title="Projects" />
       <div className="light-gray-container">
-        <div className="container w-80-centered">
-          <h1 className="heading-section">My projects</h1>
+        <div className="container w-lg-80-centered">
+          <h1 className="heading-section text-center">My projects</h1>
           <div className="row project-row">
             {data.allMarkdownRemark.edges.map(project => {
               return (
-                <div key={project.node.id} className="col-md-6 col-lg-4 ps-0">
-                  <div className="project-card">
+                <div
+                  key={project.node.id}
+                  className="col-md-6 col-lg-4 ps-lg-0"
+                >
+                  <div className="project-card mb-4 mx-lg-2">
                     <Link to={project.node.frontmatter.path}>
-                      <Img
-                        className="project-card-image"
-                        fluid={
-                          project.node.frontmatter.mainImage.childImageSharp.fluid
-                        }
-                        alt="project"
-                      />
+                      <div className="project-card-purple-bgc">
+                        <Img
+                          className="project-card-image"
+                          fluid={
+                            project.node.frontmatter.mainImage.childImageSharp
+                              .fluid
+                          }
+                          alt="project"
+                        />
+                      </div>
                     </Link>
-                    <div className="project-card-body">
+                    <div className="project-card-body text-center">
                       <h3 className="project-card-big-text">
                         {project.node.frontmatter.title}
                       </h3>
                       <p className="project-card-small-text">
                         {project.node.frontmatter.shortDescription}
                       </p>
+                      <Link
+                        className=" btn btn-purple-rounded my-2 text-center"
+                        to={project.node.frontmatter.path}
+                      >
+                        View more
+                      </Link>
                     </div>
                   </div>
                 </div>
