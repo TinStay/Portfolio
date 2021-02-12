8c826c1feb431418fc4ccdf2bb05bc0e15b59214
Martin Staykov <49690101+TinStay@users.noreply.github.com>
2021-02-12 16:59:35 +0100

    Added custom hamburger menu

--- src/components/Navigation/header.js
+++ src/components/Navigation/header.js
@@ -1,12 +1,13 @@
 import { Link } from "gatsby"
-import NavLink from './NavLink'
+import NavLink from "./NavLink"
 import Logo from "./logo"
 import { Navbar, Nav } from "react-bootstrap"
 import PropTypes from "prop-types"
 import React from "react"
+import { CgMenu } from "react-icons/cg"
+import { IconContext } from "react-icons"
 
 const Header = ({ siteTitle }) => {
-  
   return (
     <header>
       <Navbar className="navbar p-0 mt-0" expand="lg">
@@ -14,7 +15,15 @@ const Header = ({ siteTitle }) => {
           <Logo />
         </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="navbar-main " />
+        <Navbar.Toggle aria-controls="navbar-main ">
+              {/* <CgMenu className="navbar-hamburger" /> */}
+              <div className="hamburger-menu">
+                <div className="hamburger-line"></div>
+                <div className="hamburger-line"></div>
+                <div className="hamburger-line"></div>
+              </div>
+        </Navbar.Toggle>
+
         <Navbar.Collapse id="navbar-main ">
           <Nav className="ms-auto ">
             <Link className="nav-item" to="/" activeClassName="active-link">
@@ -28,7 +37,6 @@ const Header = ({ siteTitle }) => {
             >
               <span className="nav-link">Projects</span>
             </NavLink>
-            
 
             <Link
               className="nav-item"
